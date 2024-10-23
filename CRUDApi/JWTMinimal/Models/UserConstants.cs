4ddf21ceaeec6df91f6d1dafc45a602bbe04da54
prajwalverma <princeeprajwal@gmail.com>
2024-10-24 00:16:42 +0530

    Added Model files

---
+++ CRUDApi/JWTMinimal/Models/UserConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace JWTCore.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> userModels = new List<UserModel>()
+        {
+            new UserModel() { Username="pverma", Email="pverma.gmail.com", Givenname="Prajwal", Password="PVerma", Role="Administrator", Surname="verma"},
+            new UserModel() { Username="Nikki", Email="niki.gmail.com", Givenname="Nikita", Password="NIKKI", Role="Seller", Surname="Negi"}
+        };
+    }
+}
